0058accfff8f3498f20828062a7ba2d388b8ed85
Jaehwan Hyun <jec0101@naver.com>
2024-06-29 19:22:14 +0900

    update : Controller 완성

--- umc/src/main/java/com/umc/domain/comment/controller/CommentController.java
+++ umc/src/main/java/com/umc/domain/comment/controller/CommentController.java
@@ -1,11 +1,15 @@
 package com.umc.domain.comment.controller;
 
 import com.umc.common.response.ApiResponse;
+import com.umc.domain.comment.dto.CommentRequestDTO;
+import com.umc.domain.comment.entity.Comment;
 import com.umc.domain.comment.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,28 +21,32 @@ public class CommentController {
     @CrossOrigin
     @Operation(summary = "댓글 조회 API")
     @GetMapping("/posts/{postId}/comments")
-    public ApiResponse<Object> findComments(@PathVariable String postId){
-        return null;
+    public ApiResponse<List<Comment>> findComments(@PathVariable String postId){
+        return commentService.findByPostId(postId);
     }
 
     @CrossOrigin
     @Operation(summary = "댓글 작성 API")
     @PostMapping("/posts/{postId}/comments")
-    public ApiResponse<Object> writeComment(@PathVariable String postId){
-        return null;
+    public ApiResponse<String> writeComment(@PathVariable String postId,
+                                            @RequestBody CommentRequestDTO commentRequestDTO){
+        return commentService.save(postId, commentRequestDTO);
     }
 
     @CrossOrigin
     @Operation(summary = "댓글 삭제 API")
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
-    public ApiResponse<Object> deleteComment(@PathVariable String postId, @PathVariable String commentId){
-        return null;
+    public ApiResponse<Object> deleteComment(@PathVariable String postId,
+                                             @PathVariable String commentId){
+        return commentService.delete(commentId);
     }
 
     @CrossOrigin
     @Operation(summary = "댓글 수정 API")
     @PutMapping("/posts/{postId}/comments/{commentId}")
-    public ApiResponse<Object> updateComment(@PathVariable String postId, @PathVariable String commentId){
-        return null;
+    public ApiResponse<Object> updateComment(@PathVariable String postId,
+                                             @PathVariable String commentId,
+                                             @RequestBody CommentRequestDTO commentUpdateRequestDTO){
+        return commentService.update(commentId, commentUpdateRequestDTO);
     }
 }
--- umc/src/main/java/com/umc/domain/post/controller/PostController.java
+++ umc/src/main/java/com/umc/domain/post/controller/PostController.java
@@ -1,11 +1,17 @@
 package com.umc.domain.post.controller;
 
 import com.umc.common.response.ApiResponse;
+import com.umc.domain.post.dto.PostRequestDTO;
+import com.umc.domain.post.dto.PostResponseDTO;
+import com.umc.domain.post.dto.SimplePostResponseDTO;
 import com.umc.domain.post.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,36 +23,40 @@ public class PostController {
 
     @CrossOrigin
     @Operation(summary = "게시글 조회 API")
-    @GetMapping()
-    public ApiResponse<Object> findPosts(@RequestParam String params) {
-        return null;
+    @GetMapping
+    public ApiResponse<List<SimplePostResponseDTO>> findPosts(@RequestParam String params) {
+        return postService.findAll();
     }
 
     @CrossOrigin
     @Operation(summary = "특정 게시글 조회 API")
     @GetMapping("/{postId}")
-    public ApiResponse<Object> findPost(@PathVariable String postId){
-        return null;
+    public ApiResponse<PostResponseDTO> findPost(@PathVariable String postId){
+        return postService.find(postId);
     }
 
     @CrossOrigin
     @Operation(summary = "특정 게시글 삭제 API")
     @DeleteMapping("/{postId}")
-    public ApiResponse<Object> deleteById(@PathVariable String postId){
-        return null;
+    public ApiResponse<String> deleteById(@PathVariable String postId){
+        return postService.delete(postId);
     }
 
     @CrossOrigin
     @Operation(summary = "게시글 생성 API")
-    @PostMapping()
-    public ApiResponse<Object> postPost(){
-        return null;
+    @PostMapping
+    public ApiResponse<Object> postPost(@RequestParam(name = "boardId") String boardId,
+                                        @ModelAttribute PostRequestDTO postRequestDto,
+                                        @RequestParam(value = "images", required = false) MultipartFile[] images){
+        return postService.save(boardId, postRequestDto, images);
     }
 
     @CrossOrigin
     @Operation(summary = "게시글 수정 API")
-    @PutMapping()
-    public ApiResponse<Object> updatePost(){
-        return null;
+    @PutMapping("/{postId}")
+    public ApiResponse<Object> updatePost(@PathVariable String postId,
+                                          @ModelAttribute PostRequestDTO postRequestDto,
+                                          @RequestParam(value = "images", required = false) MultipartFile[] images){
+        return postService.update(postId, postRequestDto, images);
     }
 }
